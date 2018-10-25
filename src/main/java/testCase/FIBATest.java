e653cc0c2e27349b4d258321c2b250ef1d6876e1
JAir Aguirre <aguirre.jair@gmail.com>
2018-10-25 17:39:57 -0500

    Test classes were updated

--- src/main/java/testCase/FIBATest.java
+++ src/main/java/testCase/FIBATest.java
@@ -5,6 +5,7 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.Test;
 
+import generics.RedBlackNode;
 import junit.framework.TestCase;
 import model.FIBA;
 import model.Player;
@@ -16,34 +17,32 @@ public class FIBATest extends TestCase{
 	void stageOne() {
 		
 		fiba = new FIBA();
-		player1 =  new Player("2000", "BlueBirds", "Cami", 23, 3, 4, 34, 21, 12, 1);
+		player1 =  new Player("2000", "BlueBirds", "Cami", 23, 3, 4, 34, 21, 12, 3);
 		
 	}
 
-	@Test
-	
-	public void testGgetLess() {
-		 stageOne();
-		 
-		 
-		
-		
-	}
-	
-	
-	@Test 
-	public void testGetSame() {
-		 stageOne();
-		
-	}
-	
 	@Test
 	public void testAddnewPlayer() {
 		
 		 stageOne();
 		fiba.addNewPlayer(player1);
-		 
+		
+		RedBlackNode<Player> pla=  fiba.getTsT().getRoot();
+	
+		assertEquals( fiba.getTsT().getRoot(), pla);
 		
 		 
+	}
+	
+	@Test
+	public void testdelete() {
+		
+		stageOne();
+		
+		fiba.addNewPlayer(player1);
+		fiba.delete(player1);
+		
+		assertEquals( fiba.getTsT().getRoot(), null);
+		
 	}
 }
---
+++ src/main/java/testCase/RedBlackTreeTest.java
@@ -0,0 +1,21 @@
+package testCase;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.Test;
+
+import generics.RedBlackTree;
+import junit.framework.TestCase;
+
+class RedBlackTreeTest extends TestCase {
+
+	private RedBlackTree<String> redblackTree;
+	
+	void stageOne(){
+		
+	redblackTree = new RedBlackTree<>();
+	
+		
+	}
+}
