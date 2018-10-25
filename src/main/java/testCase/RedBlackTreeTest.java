0f03b49b32e27e143cf39c16c5ded552dd1fd1e3
JAir Aguirre <aguirre.jair@gmail.com>
2018-10-25 18:16:29 -0500

    RedBlackTree's testCases was updated

--- src/main/java/testCase/RedBlackTreeTest.java
+++ src/main/java/testCase/RedBlackTreeTest.java
@@ -11,11 +11,32 @@ import junit.framework.TestCase;
 class RedBlackTreeTest extends TestCase {
 
 	private RedBlackTree<String> redblackTree;
-	
+	private String refers;
 	void stageOne(){
 		
 	redblackTree = new RedBlackTree<>();
-	
+	refers = "test";
 		
 	}
+	
+	
+	@Test
+	
+	public void testInsert() {
+		
+		redblackTree.insertRB(refers);
+		
+		assertEquals(redblackTree.getRoot(), refers);
+	}
+	
+	@Test
+	public void testsDelete() {
+		
+		redblackTree.insertRB(refers);
+		redblackTree.deleteRB(refers);
+		
+		assertEquals(redblackTree.getRoot(), null);
+	}
+	
+	
 }
